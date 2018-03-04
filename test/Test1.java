d7b04792a1b6cb5d110eb255e70f840c9d073db4
learnless <b13502518214@163.com>
2018-03-04 09:14:21 +0800

    算法与数据结构分析 Java语言描述 第三版

--- test/Test1.java
+++ test/Test1.java
@@ -33,6 +33,21 @@ public class Test1 {
 
     }
 
+    @Test
+    public void t4() throws Exception {
+        int i = 0;
+        for (print('A'); print('C') && i < 2; print('D')) {
+            i++;
+            System.out.print('T');
+            print('B');
+        }
+    }
+
+    private static boolean print(char c) {
+        System.out.print(c);
+        return true;
+    }
+
     private static int[] sort(int[] a) {
         a[0] = 2;
         return null;
